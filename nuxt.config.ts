fef695aee7247816f507f451ef67db59ec90bd37
HideInMatrix <davidmorgancreate@gmail.com>
2025-11-24 22:17:50 +0800

    test: read config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,17 +8,17 @@ export default defineNuxtConfig({
     "@nuxt/scripts",
     "@unocss/nuxt",
     "shadcn-nuxt",
-    '@vueuse/nuxt',
+    "@vueuse/nuxt",
     "nuxt-auth-utils",
   ],
-  
-  runtimeConfig:{
-    public:{
+
+  runtimeConfig: {
+    public: {
       tgToken: process.env.NUXT_PUBLIC_TG_BOT_TOKEN,
       tgChatId: process.env.NUXT_PUBLIC_TG_CHAT_ID,
       allowHosts: process.env.NUXT_PUBLIC_TG_ALLOW_WEBSITE,
       account: process.env.NUXT_PUBLIC_ACCOUNT,
-      password: process.env.NUXT_PUBLIC_PASSWORD
+      password: process.env.NUXT_PUBLIC_PASSWORD,
     },
   },
   shadcn: {
