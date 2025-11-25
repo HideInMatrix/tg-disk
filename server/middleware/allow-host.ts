e00f4c0c2a1aed99f7fd6a339f24a587a2b4b252
HideInMatrix <davidmorgancreate@gmail.com>
2025-11-25 20:32:30 +0800

    test: img show

--- server/middleware/allow-host.ts
+++ server/middleware/allow-host.ts
@@ -1,21 +1,22 @@
-import { createReadStream } from 'node:fs';
-import { join } from 'node:path';
-import { defineEventHandler, sendStream } from 'h3';
-
+import { createReadStream } from "node:fs";
+import { join } from "node:path";
+import { defineEventHandler, sendStream } from "h3";
 
 export default defineEventHandler((event) => {
-    const config = useRuntimeConfig()
-    const headers = event.node.req.headers;
-    
-    const host = headers['host'];
-    const allowHosts = [...config.public.allowHosts.split(",")]
-    console.log("访问域名",host)
-    console.log((allowHosts.length > 0) , event.path.startsWith('/file') , !!!host , !allowHosts.includes(host!))
-    // 只有设置了白名单才允许进行检测
-    if ((allowHosts.length > 0) && event.path.startsWith('/file') && (!!!host || !allowHosts.includes(host!))) {
-        const filePath = join(process.cwd(), 'public/placeholder.svg');
-        setHeader(event, "Content-Type", "image/svg+xml");
-        setHeader(event, "Cache-Control", "public, max-age=86400000"); // Cache for 1 day
-        return sendStream(event, createReadStream(filePath));
-    }
+  const config = useRuntimeConfig();
+  const headers = event.node.req.headers;
+
+  const host = headers["host"];
+  const allowHosts = config.public.allowHosts?.split(",") || [];
+  // 只有设置了白名单才允许进行检测
+  if (
+    !!config.public.allowHosts &&
+    event.path.startsWith("/file") &&
+    (!!!host || !allowHosts.includes(host!))
+  ) {
+    const filePath = join(process.cwd(), "public/placeholder.svg");
+    setHeader(event, "Content-Type", "image/svg+xml");
+    setHeader(event, "Cache-Control", "public, max-age=86400000"); // Cache for 1 day
+    return sendStream(event, createReadStream(filePath));
+  }
 });
