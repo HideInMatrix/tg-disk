d1db0a7a68f665bfde06beaf4ff153497ac4b137
DipperMorgan <99717719+HideInMatrix@users.noreply.github.com>
2025-11-28 13:46:40 +0800

    feat: 更新中间件判断方法

--- server/middleware/allow-host.ts
+++ server/middleware/allow-host.ts
@@ -1,23 +1,30 @@
-import { createReadStream } from "node:fs";
-import { join } from "node:path";
-import { defineEventHandler, sendStream } from "h3";
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+import { defineEventHandler, sendStream, setHeader, getHeader } from 'h3';
 
 export default defineEventHandler((event) => {
   const config = useRuntimeConfig();
-  const headers = event.node.req.headers;
 
-  const host = headers["host"];
-  const allowHosts = config.public.allowHosts?.split(",") || [];
-  // 只有设置了白名单才允许进行检测
-  console.log(!!config.public.allowHosts,event.path.startsWith("/file"),host,!allowHosts.includes(host!))
-  if (
-    !!config.public.allowHosts &&
-    event.path.startsWith("/file") &&
-    (!!!host || !allowHosts.includes(host!))
-  ) {
-    const filePath = join(process.cwd(), "public/placeholder.svg");
+  // 获取 Host（去除端口）
+  const host = getHeader(event, 'host')?.split(':')[0] ?? "";
+
+  // 获取白名单（过滤空值）
+  const allowHosts = config.public.allowHosts
+    ?.split(',')
+    .map(h => h.trim())
+    .filter(Boolean) ?? [];
+
+  // 判断是否不在白名单
+  const isBlocked =
+    config.public.allowHosts &&           // 开启白名单检测
+    event.path.startsWith("/file") &&     // 只处理 /file
+    (!host || !allowHosts.includes(host)); // host 不合法
+
+
+  if (isBlocked) {
+    const filePath = join(process.cwd(), 'public/placeholder.svg');
     setHeader(event, "Content-Type", "image/svg+xml");
-    setHeader(event, "Cache-Control", "public, max-age=86400000"); // Cache for 1 day
+    setHeader(event, "Cache-Control", "public, max-age=3600");
     return sendStream(event, createReadStream(filePath));
   }
 });
