265cbc39818c9a753a397745527be9e86e8c9ae4
HideInMatrix <davidmorgancreate@gmail.com>
2025-11-28 22:48:26 +0800

    fix: cloudflare deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
     componentDir: "./app/components/ui",
   },
   nitro: {
-    preset: "cloudflare_module",
+    preset: "cloudflare-pages",
     prerender: {
       autoSubfolderIndex: false, // 这个是官方建议，匹配 Cloudflare 的路由行为
     },
